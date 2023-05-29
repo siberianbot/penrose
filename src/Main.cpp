ef9468702e823a615b4dcd1164fe77f4b979b371
Nikita Kozhevnikov <nkozhevnikov@outlook.com>
2023-05-29 15:23:00 +0700

    Added error handling for easy debugging

---
+++ src/Main.cpp
@@ -0,0 +1,57 @@
+#include <csignal>
+#include <iostream>
+#include <string>
+
+#include "src/Core/Engine.hpp"
+#include "src/Rendering/RenderContext.hpp"
+
+void printError(const std::exception &error, int level) {
+    if (level > 1) {
+        std::cerr << std::string(2 * level, ' ');
+    }
+
+    if (level != 0) {
+        std::cerr << "-> ";
+    }
+
+    std::cerr << error.what() << std::endl;
+
+    try {
+        std::rethrow_if_nested(error);
+    } catch (const std::exception &nested) {
+        printError(nested, level + 1);
+    }
+}
+
+int main() {
+    std::set_terminate([]() {
+        auto caught = std::current_exception();
+
+        if (caught) {
+            std::cerr << "Unhandled engine exception" << std::endl;
+
+            try {
+                std::rethrow_exception(caught);
+            } catch (const std::exception &error) {
+                printError(error, 0);
+            }
+        } else {
+            std::cerr << "Abnormal termination" << std::endl;
+        }
+
+#if defined(SIGTRAP)
+        std::raise(SIGTRAP);
+#endif
+
+        std::abort();
+    });
+
+    Penrose::Engine engine;
+
+    auto graph = Penrose::makeDefaultRenderGraph();
+    engine.resources().get<Penrose::RenderContext>()->setRenderGraph(graph);
+
+    engine.run();
+
+    return 0;
+}
