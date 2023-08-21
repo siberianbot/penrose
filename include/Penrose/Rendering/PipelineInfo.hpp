3c789f2b14f7240953c929d7f2fd354bbadd24ad
Nikita Kozhevnikov <nkozhevnikov@outlook.com>
2023-08-21 13:31:05 +0700

    Moved implementations of PipelineInfo classes methods to header for allowing usage in const-expressions

--- include/Penrose/Rendering/PipelineInfo.hpp
+++ include/Penrose/Rendering/PipelineInfo.hpp
@@ -12,20 +12,6 @@ namespace Penrose {
         Fragment
     };
 
-    class PipelineShaderStage {
-    public:
-        [[nodiscard]] PipelineShaderStage &setType(PipelineShaderStageType type);
-        [[nodiscard]] PipelineShaderStage &setShaderAssetName(const std::string &shaderAssetName);
-
-        [[nodiscard]] const PipelineShaderStageType &getType() const { return this->_type; }
-
-        [[nodiscard]] const std::string &getShaderAssetName() const { return this->_shaderAssetName; }
-
-    private:
-        PipelineShaderStageType _type;
-        std::string _shaderAssetName;
-    };
-
     enum class PipelineBindingInputRate {
         Vertex,
         Instance
@@ -36,14 +22,60 @@ namespace Penrose {
         Vec3
     };
 
+    class PipelineShaderStage {
+    public:
+        explicit constexpr PipelineShaderStage(PipelineShaderStageType type,
+                                               std::string &&shaderAssetName)
+                : _type(type),
+                  _shaderAssetName(shaderAssetName) {
+            //
+        }
+
+        [[nodiscard]] constexpr PipelineShaderStage &setType(PipelineShaderStageType type) {
+            this->_type = type;
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineShaderStage &setShaderAssetName(const std::string &shaderAssetName) {
+            this->_shaderAssetName = shaderAssetName;
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr const PipelineShaderStageType &getType() const { return this->_type; }
+
+        [[nodiscard]] constexpr const std::string &getShaderAssetName() const { return this->_shaderAssetName; }
+
+    private:
+        PipelineShaderStageType _type;
+        std::string _shaderAssetName;
+    };
+
     class PipelineBindingAttribute {
     public:
-        [[nodiscard]] PipelineBindingAttribute &setFormat(PipelineBindingAttributeFormat format);
-        [[nodiscard]] PipelineBindingAttribute &setOffset(std::uint32_t offset);
+        explicit constexpr PipelineBindingAttribute(PipelineBindingAttributeFormat format,
+                                                    std::uint32_t offset)
+                : _format(format),
+                  _offset(offset) {
+            //
+        }
 
-        [[nodiscard]] const PipelineBindingAttributeFormat &getFormat() const { return this->_format; }
+        [[nodiscard]] constexpr PipelineBindingAttribute &setFormat(PipelineBindingAttributeFormat format) {
+            this->_format = format;
 
-        [[nodiscard]] const std::uint32_t &getOffset() const { return this->_offset; }
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineBindingAttribute &setOffset(std::uint32_t offset) {
+            this->_offset = offset;
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr const PipelineBindingAttributeFormat &getFormat() const { return this->_format; }
+
+        [[nodiscard]] constexpr const std::uint32_t &getOffset() const { return this->_offset; }
 
     private:
         PipelineBindingAttributeFormat _format;
@@ -52,15 +84,46 @@ namespace Penrose {
 
     class PipelineBinding {
     public:
-        [[nodiscard]] PipelineBinding &setInputRate(PipelineBindingInputRate inputRate);
-        [[nodiscard]] PipelineBinding &setStride(std::uint32_t stride);
-        [[nodiscard]] PipelineBinding &addAttribute(PipelineBindingAttribute &&attribute);
+        explicit constexpr PipelineBinding(PipelineBindingInputRate inputRate,
+                                           std::uint32_t stride,
+                                           std::vector<PipelineBindingAttribute> &&attributes = {})
+                : _inputRate(inputRate),
+                  _stride(stride),
+                  _attributes(attributes) {
+            //
+        }
 
-        [[nodiscard]] const PipelineBindingInputRate &getInputRate() const { return this->_inputRate; }
+        [[nodiscard]] constexpr PipelineBinding &setInputRate(PipelineBindingInputRate inputRate) {
+            this->_inputRate = inputRate;
 
-        [[nodiscard]] const std::uint32_t &getStride() const { return this->_stride; }
+            return *this;
+        }
 
-        [[nodiscard]] const std::vector<PipelineBindingAttribute> &getAttributes() const { return this->_attributes; }
+        [[nodiscard]] constexpr PipelineBinding &setStride(std::uint32_t stride) {
+            this->_stride = stride;
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineBinding &addAttribute(const PipelineBindingAttribute &attribute) {
+            this->_attributes.push_back(attribute);
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineBinding &addAttribute(PipelineBindingAttribute &&attribute) {
+            this->_attributes.push_back(attribute);
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr const PipelineBindingInputRate &getInputRate() const { return this->_inputRate; }
+
+        [[nodiscard]] constexpr const std::uint32_t &getStride() const { return this->_stride; }
+
+        [[nodiscard]] constexpr const std::vector<PipelineBindingAttribute> &getAttributes() const {
+            return this->_attributes;
+        }
 
     private:
         PipelineBindingInputRate _inputRate;
@@ -70,12 +133,40 @@ namespace Penrose {
 
     class PipelineInfo {
     public:
-        [[nodiscard]] PipelineInfo &addStage(PipelineShaderStage &&stage);
-        [[nodiscard]] PipelineInfo &addBinding(PipelineBinding &&binding);
+        explicit constexpr PipelineInfo(std::vector<PipelineShaderStage> &&stages = {},
+                                        std::vector<PipelineBinding> &&bindings = {})
+                : _stages(stages),
+                  _bindings(bindings) {
+            //
+        }
 
-        [[nodiscard]] const std::vector<PipelineShaderStage> &getStages() const { return this->_stages; }
+        [[nodiscard]] constexpr PipelineInfo &addStage(const PipelineShaderStage &stage) {
+            this->_stages.push_back(stage);
 
-        [[nodiscard]] const std::vector<PipelineBinding> &getBindings() const { return this->_bindings; }
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineInfo &addStage(PipelineShaderStage &&stage) {
+            this->_stages.push_back(stage);
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineInfo &addBinding(const PipelineBinding &binding) {
+            this->_bindings.push_back(binding);
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr PipelineInfo &addBinding(PipelineBinding &&binding) {
+            this->_bindings.push_back(binding);
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr const std::vector<PipelineShaderStage> &getStages() const { return this->_stages; }
+
+        [[nodiscard]] constexpr const std::vector<PipelineBinding> &getBindings() const { return this->_bindings; }
 
     private:
         std::vector<PipelineShaderStage> _stages;
